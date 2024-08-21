4f70a0011ff5225ff248979f9d5d8eb19da64045
Vishwanathhatti <92014314+Vishwanathhatti@users.noreply.github.com>
2024-08-21 17:10:38 +0530

    Update index.js

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ app.use(express.json())
 app.use(express.urlencoded({extended:true}));
 app.use(cookieParser());
 const corsOptions = {
-    origin:'http://localhost:5173',
+    origin:'https://phenomenal-arithmetic-119554.netlify.app',
     credentials:true
 }
 app.use(cors(corsOptions))
